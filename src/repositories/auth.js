d282b498c3147df1a2609796ed25c367f0b09b09
Taisei <taisei.960420@gmail.com>
2024-08-19 18:59:31 +0900

    グローバルステートでユーザーのログイン状態を管理

--- src/repositories/auth.js
+++ src/repositories/auth.js
@@ -9,4 +9,25 @@ export const authRepository = {
     if (error != null) throw new Error(error.message);
     return { ...data.user, userName: data.user.user_metadata.name };
   },
+  async signin(email, password) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) throw new Error(error.message);
+    return {
+      ...data.user,
+      userName: data.user.user_metadata.name,
+    }
+  },
+  async getCurrentUser() {
+    const { data, error } = await supabase.auth.getSession();
+    if (error != null) throw new Error(error.message);
+    if (data.session == null) return;
+
+    return {
+      ...data.session.user,
+      userName: data.session.user.user_metadata.name,
+    }
+  }
 }
